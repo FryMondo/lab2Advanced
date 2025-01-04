ff8f3b10b15f06cd1e69aa926065588470a73af3
FryMondo <ptachyksergei@gmail.com>
2025-01-04 13:21:50 +0200

    Represents a user entity

---
+++ src/User.java
@@ -0,0 +1,18 @@
+// Приклади класів сутностей
+@Table(name = "users")
+class User {
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
